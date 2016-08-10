6a7c45d1023b87c7d5cb7164716f64a1e6a920cb
Jess Scheuring <jscheuring@gmail.com>
2016-08-10 09:34:02 -0500

    initial commit

---
+++ app/scripts/entry.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, Route, hashHistory, browserHistory, Link, IndexRoute} from 'react-router';
+
+const App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <nav>
+          <Link to="/">Go home</Link>
+          <Link to="/test">Go to test</Link>
+        </nav>
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
+const Home = React.createClass({
+  render: function() {
+    return (
+      <h1>Home!</h1>
+    )
+  }
+})
+
+const Test = React.createClass({
+  render: function() {
+    return (
+      <h1>Test!</h1>
+    )
+  }
+})
+
+const router = (
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+    <IndexRoute component={Home}/>
+    <Route path="/test" component={Test}/>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('container'));
